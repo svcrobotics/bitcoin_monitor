772d9eac4ff81bfe254c2a3a7ffadca11154bd5f
Victor Perez <svcrobotics@gmail.com>
2026-04-25 11:34:50 +0200

    Build book with realtime and Git chapters

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f2a74e70.js',
+                path_to_root + 'searchindex-1a9a0f96.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
