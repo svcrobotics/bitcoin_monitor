ce227de9982115925c454e55f3580eb2ce2f9ae2
Victor Perez <svcrobotics@gmail.com>
2026-05-10 14:01:16 +0200

    Add layer1 architecture chapter

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-29121e87.js',
+                path_to_root + 'searchindex-dd9a9f14.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
