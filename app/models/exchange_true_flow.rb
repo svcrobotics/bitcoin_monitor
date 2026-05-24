312779af7c085500af8b3e60bec14bfa9829363f
Victor Perez <svcrobotics@gmail.com>
2026-05-24 15:38:01 +0200

    Remove legacy inflow outflow pipeline

--- app/models/exchange_true_flow.rb
+++
@@ -1,7 +0,0 @@
-# app/models/exchange_true_flow.rb
-class ExchangeTrueFlow < ApplicationRecord
-  STATUSES = %w[green amber red].freeze
-  validates :day, presence: true, uniqueness: true
-  validates :status, inclusion: { in: STATUSES }, allow_nil: true
-  scope :recent, -> { order(day: :desc) }
-end
