52fddf500963e0224c549f7539f6f1dbf0270d5f
Victor Perez <svcrobotics@gmail.com>
2026-07-16 20:43:19 +0200

    Make legacy Sidekiq recovery opt in

--- config/initializers/sidekiq_recovery.rb
+++ config/initializers/sidekiq_recovery.rb
@@ -1,33 +1,56 @@
 # config/initializers/sidekiq_recovery.rb
 # frozen_string_literal: true
 
-if defined?(Sidekiq)
-  Sidekiq.configure_server do |config|
-    config.on(:startup) do
-      begin
-        Sidekiq.redis do |redis|
-          lock_key = "recovery:startup_enqueue_lock"
+module TansaLegacySidekiqRecovery
+  ENV_KEY = "TANSA_LEGACY_SIDEKIQ_RECOVERY_ENABLED"
+  TRUE_VALUES = %w[1 true yes on].freeze
+  FALSE_VALUES = %w[0 false no off].freeze
 
-          locked =
-            redis.set(
-              lock_key,
-              Process.pid,
-              nx: true,
-              ex: 60
-            )
+  module_function
 
-          unless locked
-            Rails.logger.info("[recovery] startup enqueue skipped: lock already present")
-            next
+  def enabled?(value = ENV[ENV_KEY])
+    normalized = value.to_s.strip.downcase
+    return false if normalized.empty? || FALSE_VALUES.include?(normalized)
+    return true if TRUE_VALUES.include?(normalized)
+
+    raise ArgumentError, "invalid #{ENV_KEY} value"
+  end
+
+  def install!
+    return false unless enabled?
+    return false unless defined?(Sidekiq)
+
+    Sidekiq.configure_server do |config|
+      config.on(:startup) do
+        begin
+          Sidekiq.redis do |redis|
+            lock_key = "recovery:startup_enqueue_lock"
+
+            locked =
+              redis.set(
+                lock_key,
+                Process.pid,
+                nx: true,
+                ex: 60
+              )
+
+            unless locked
+              Rails.logger.info("[recovery] startup enqueue skipped: lock already present")
+              next
+            end
+
+            Rails.logger.info("[recovery] startup enqueue")
+
+            RecoveryOrchestratorJob.perform_later
           end
-
-          Rails.logger.info("[recovery] startup enqueue")
-
-          RecoveryOrchestratorJob.perform_later
+        rescue => e
+          Rails.logger.error("[recovery] startup enqueue failed #{e.class} #{e.message}")
         end
-      rescue => e
-        Rails.logger.error("[recovery] startup enqueue failed #{e.class} #{e.message}")
       end
     end
+
+    true
   end
 end
+
+TansaLegacySidekiqRecovery.install!
